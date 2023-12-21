cd54944b4f125486647c71f9c34a129774e80a4f
hammadShamir <hammadshamir7@gmail.com>
2023-12-21 14:24:31 +0500

    Added Nodemon

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 mongo_url = process.env.MONGO_URL;
 
 mongoose
-  .connect(mongo_url, { useNewUrlParser: true })
+  .connect(mongo_url)
   .then(() => {
     console.log("Connected to the Database");
   })
