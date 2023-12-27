894de5b2bedb2a571257d40eda17091e37b89262
rashidjatoi <rashidjatoi31@gmail.com>
2023-12-27 16:10:53 +0500

    Reviews Updated

--- src/router/reviews_router.js
+++
@@ -1,8 +0,0 @@
-const reviewsRouter = require("express").Router();
-const reviewsController = require("../controller/reviews_controller");
-const { check } = require("express-validator");
-
-reviewsRouter.get("/reviews/:hostelId", reviewsController.getReviewsByHostelId);
-reviewsRouter.get("/reviews", reviewsController.getUserReview);
-
-module.exports = reviewsRouter;
