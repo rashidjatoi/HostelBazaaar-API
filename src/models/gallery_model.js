dc129d58f6c28a87108f9d0ae828aa9fb4d5c80d
rashidjatoi <rashidjatoi31@gmail.com>
2023-12-27 18:18:12 +0500

    Gallery Branch

---
+++ src/models/gallery_model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const gallerySchema = mongoose.Schema({
+  hostelId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "hostel",
+    required: true,
+  },
+
+  images: Array,
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("gallery", gallerySchema);
