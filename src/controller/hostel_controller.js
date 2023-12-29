e2355311c4a26672c1e69cdf8050a9d6c14afe28
hammadShamir <hammadshamir7@gmail.com>
2023-12-29 21:07:39 +0500

    Added user Id in hostelModel

--- src/controller/hostel_controller.js
+++ src/controller/hostel_controller.js
@@ -14,8 +14,9 @@ const hostelController = {
           .status(400)
           .json({ errors: errors.array().map((err) => err.msg) });
       } else {
-        const { thumbnail, title, desc, price, location, rating } = req.body;
+        const { userId, thumbnail, title, desc, price, location, rating } = req.body;
         await HostelModel.create({
+          userId: userId,
           thumbnail: thumbnail,
           title: title,
           desc: desc,
@@ -60,23 +61,23 @@ const hostelController = {
           date: element.date,
           amentities: amenities
             ? {
-                freeWifi: amenities.freeWifi,
-                privateBathroom: amenities.privateBathroom,
-                freeParking: amenities.freeParking,
-                helpDesk: amenities.helpDesk,
-                airCondition: amenities.airCondition,
-                keyAccess: amenities.keyAccess,
-                transportation: amenities.transportation,
-              }
+              freeWifi: amenities.freeWifi,
+              privateBathroom: amenities.privateBathroom,
+              freeParking: amenities.freeParking,
+              helpDesk: amenities.helpDesk,
+              airCondition: amenities.airCondition,
+              keyAccess: amenities.keyAccess,
+              transportation: amenities.transportation,
+            }
             : null,
           reviews: hostelReviews
             ? {
-                cleanliness: hostelReviews.cleanliness,
-                amenities: hostelReviews.cleanliness,
-                location: hostelReviews.cleanliness,
-                comfort: hostelReviews.cleanliness,
-                wifi: hostelReviews.cleanliness,
-              }
+              cleanliness: hostelReviews.cleanliness,
+              amenities: hostelReviews.cleanliness,
+              location: hostelReviews.cleanliness,
+              comfort: hostelReviews.cleanliness,
+              wifi: hostelReviews.cleanliness,
+            }
             : null,
 
           images: gallery ? gallery.images : null,
